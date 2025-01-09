d82edcc3707d84597d88d2412d030933682dbf0e
Muhammad Mudassar <mmudassards@gmail.com>
2025-01-09 17:51:59 +0000

    added another approch.

--- Arrays/remove-duplicates-from-sorted-array.py
+++ Arrays/remove-duplicates-from-sorted-array.py
@@ -6,3 +6,9 @@ class Solution:
                 nums[l] = nums[r]
                 l += 1
         return l
+################################################################
+class Solution:
+    def removeDuplicates(self, nums: list[int]) -> int:
+        unique = sorted(set(nums))
+        nums[:len(unique)] = unique
+        return len(unique)
